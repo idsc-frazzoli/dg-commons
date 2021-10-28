5489be779c4039cd03327c673dfa102ade055b45
Zelio Suter <suterz@ethz.ch>
2021-10-28 16:52:34 +0200

    initial commit for pull request

---
+++ src/dg_commons_dev/controllers/steering_controllers.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import Optional, Union, List
+from dg_commons_dev.controllers.controller_types import SteeringController, SteeringControllerParam
+
+
+@dataclass
+class SCIdentityParam(SteeringControllerParam):
+    pass
+
+
+class SCIdentity(SteeringController):
+    def __init__(self, params: Optional[SCIdentityParam] = None):
+        self.params = SCIdentityParam() if params is None else params
+        super().__init__()
+
+    def _get_steering_vel(self, current_steering: float) -> float:
+        return self.delta_ref
+
+
+@dataclass
+class SCPParam(SteeringControllerParam):
+    ddelta_kp: Union[List[float], float] = 10
+
+
+class SCP(SteeringController):
+    def __init__(self, params: Optional[SCPParam] = None):
+        self.params = SCPParam() if params is None else params
+        super().__init__()
+
+    def _get_steering_vel(self, current_steering: float) -> float:
+        return self.params.ddelta_kp * (self.delta_ref - current_steering)
