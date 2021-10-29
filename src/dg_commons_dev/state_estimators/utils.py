d0a118727a2ca88ace6c5aa17b6654aea9f7ba91
Zelio Suter <suterz@ethz.ch>
2021-10-29 15:05:54 +0200

    clean utils up

--- src/dg_commons_dev/state_estimators/utils.py
+++ src/dg_commons_dev/state_estimators/utils.py
@@ -6,9 +6,12 @@ from dg_commons_dev.utils import BaseParams
 
 @dataclass
 class ExponentialParams(BaseParams):
-    """ Exponential Distribution Parameters """
+    """
+    Exponential Distribution Parameters
+    """
 
     lamb: Union[List[float], float] = 1
+    """ lambda > 0 """
 
     def __post_init__(self):
         if isinstance(self.lamb, list):
@@ -20,17 +23,32 @@ class ExponentialParams(BaseParams):
 
 
 class Exponential:
-    """ Exponential Distribution """
+    """
+    Exponential Distribution Parameters
+
+    f(t) = lambda * exp(- lambda * t) if t >= 0
+    f(t) = 0 otherwise
+    """
 
     def __init__(self, params: ExponentialParams):
         self.params = params
 
     def cdf(self, t):
+        """
+        Cumulative distribution function
+        @param t: val
+        @return: evaluation at t
+        """
         assert t >= 0
 
         return 1 - math.exp(-self.params.lamb * t)
 
     def pdf(self, t):
+        """
+        Probability density function
+        @param t: val
+        @return: evaluation at t
+        """
         assert t >= 0
 
         return self.params.lamb * math.exp(-self.params.lamb * t)
