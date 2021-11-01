cd4bb6fd1024a9fd4fa50741b2c352cf969b0bec
Zelio Suter <suterz@ethz.ch>
2021-11-01 13:48:55 +0100

    minor fixes and clean up

--- src/dg_commons_dev/controllers/steering_controllers.py
+++ src/dg_commons_dev/controllers/steering_controllers.py
@@ -4,23 +4,28 @@ from dg_commons_dev.controllers.controller_types import SteeringController
 from dg_commons_dev.utils import BaseParams
 
 
-@dataclass
-class SCIdentityParam(BaseParams):
-    pass
-
-
 class SCIdentity(SteeringController):
-    def __init__(self, params: Optional[SCIdentityParam] = None):
-        self.params = SCIdentityParam() if params is None else params
+    def __init__(self, params: Optional[BaseParams] = None):
+        self.params = BaseParams() if params is None else params
         super().__init__()
 
     def _get_steering_vel(self, current_steering: float) -> float:
+        """
+        Returns delta ref itself.
+        @param current_steering: Current steering angle
+        @return: Desired steering velocity
+        """
         return self.delta_ref
 
 
 @dataclass
 class SCPParam(BaseParams):
+    """
+    Parameters of P - steering controller
+    """
+
     ddelta_kp: Union[List[float], float] = 10
+    """ Kappa p - parameter """
 
 
 class SCP(SteeringController):
@@ -29,4 +34,9 @@ class SCP(SteeringController):
         super().__init__()
 
     def _get_steering_vel(self, current_steering: float) -> float:
+        """
+        Computes steering velocity with a p-controller
+        @param current_steering: Current steering angle
+        @return: Desired steering velocity
+        """
         return self.params.ddelta_kp * (self.delta_ref - current_steering)
