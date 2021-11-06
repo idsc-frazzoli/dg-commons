027261a9229f2e07695bb56450370274bd23c60f
alezana <azanardi@ethz.ch>
2021-11-06 10:03:53 +0100

    fix

--- src/dg_commons_tests/test_sim/test_simple_sim.py
+++ src/dg_commons_tests/test_sim/test_simple_sim.py
@@ -58,8 +58,6 @@ def get_simple_scenario() -> SimContext:
 
 def generate_report(sim_context: SimContext) -> Report:
     r = Report("EpisodeVisualisation")
-    if sim_context.sim_terminated is not True:
-        raise RuntimeWarning("Generating a simulation report from a simulation that is not terminated")
     gif_viz = r.figure(cols=1)
     with gif_viz.data_file("Animation", MIME_GIF) as fn:
         create_animation(file_path=fn, sim_context=sim_context, figsize=(16, 8), dt=20, dpi=120, plot_limits="auto")
