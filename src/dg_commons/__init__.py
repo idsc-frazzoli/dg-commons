311bec6c781dc48b6a4006a9b38fd50d91ba9fef
Alessandro Zanardi <azanardi@ethz.ch>
2022-02-17 15:54:08 +0100

    Dev az012 (#32)

--- src/dg_commons/__init__.py
+++ src/dg_commons/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "0.0.19"
 
 from logging import INFO
+from typing import ClassVar
 
 import contracts
 from zuper_commons.logs import ZLogger
@@ -10,6 +11,17 @@ logger = ZLogger(__name__)
 logger.setLevel(level=INFO)
 
 contracts.disable_all()
+
+
+class DgCommonsConstants:
+    """Global constants for the library."""
+
+    checks: ClassVar[bool] = True
+    """
+        If true activates extra safety checks and assertions.
+        Mainly on object creations and methods.
+    """
+
 
 from .seq import *
 from .geo import *
