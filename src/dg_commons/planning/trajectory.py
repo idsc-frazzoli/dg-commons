96d8c856e8683b89fc57b27b48f92040d824a917
Leon Zueger <leonzueger@bluewin.ch>
2022-03-02 11:04:38 +0100

    Minors for starting to solve trajectory game

--- src/dg_commons/planning/trajectory.py
+++ src/dg_commons/planning/trajectory.py
@@ -1,10 +1,11 @@
 from dataclasses import replace
 from functools import partial
+from itertools import product
 from typing import List, Optional, Type, Mapping, Set, Iterator
 
 import numpy as np
 from geometry import xytheta_from_SE2
-from networkx import DiGraph
+from networkx import DiGraph, is_directed_acyclic_graph, all_simple_paths, descendants, has_path, shortest_path
 
 from dg_commons import SE2Transform, PlayerName, X
 from dg_commons.seq.sequence import DgSampledSequence, iterate_with_dt
@@ -73,9 +74,72 @@ class TrajectoryGraph(DiGraph):
     pass
     # todo missing sampling time to have proper trajectories
 
+    def add_node(self, state: VehicleState, **attr):
+        super(TrajectoryGraph, self).add_node(node_for_adding=state, **attr)
+
+    def check_node(self, node: VehicleState):
+        if node not in self.nodes:
+            raise ValueError(f"{node} not in graph!")
+
+    def add_edge(self, trajectory: Trajectory, **attr):
+        source, target = trajectory.at(trajectory.get_start()), trajectory.at(trajectory.get_end())
+        self.check_node(node=source)
+        attr["transition"] = trajectory
+        if target not in self.nodes:
+            self.add_node(state=target, gen=self.nodes[source]["gen"] + 1)
+
+        super(TrajectoryGraph, self).add_edge(u_of_edge=source, v_of_edge=target, **attr)
+        #self.trajectories[(source, target)] = trajectory
+
+
+    def get_max_gen(self):
+        gens = [node ["gen"] for node in self.nodes]
+        return max(gens)
+
     # todo
     def get_all_trajectories(self) -> Set[Trajectory]:
+        assert is_directed_acyclic_graph(self)
+        ret = set()
+
+        max_gen = self.get_max_gen()
+        start_nodes = [node for node in self.nodes if node["gen"] == 0]
+        end_nodes = [node for node in self.nodes if node["gen"] == max_gen]
+        all_paths = []
+        for (start, end) in product(start_nodes, end_nodes):
+            all_paths.append(all_simple_paths(self, start, end))
+        return all_paths
+
+
+
+
         pass
 
+    def get_trajectory(self, source: VehicleState, target: VehicleState) -> Trajectory:
+        self.check_node(source)
+        self.check_node(target)
+        if not has_path(G=self, source=source, target=target):
+            raise ValueError(f"No path exists between {source, target}!")
+
+        nodes = shortest_path(G=self, source=source, target=target)
+        traj: List[Trajectory] = [] # todo how to fix this
+        for node1, node2 in zip(nodes[:-1], nodes[1:]):
+            traj += self.get_edge_data(u=node1, v=node2)["transition"]
+            #traj.append(self.get_trajectory_edge(source=node1, target=node2))
+        #return Trajectory(values=traj, lane=self.lane, goal=self.goal)
+        return traj[0]
+
+
     def iterate_all_trajectories(self) -> Iterator[Trajectory]:
+        all_traj = self.get_all_trajectories()
+        # todo
         pass
+
+
+
+
+def get_all_trajectories(self, source: VehicleState) -> Set[Trajectory]:
+    if source not in self.nodes:
+        raise ValueError(f"Source node ({source}) not in graph!")
+
+    successors = [self.get_trajectory_edge(source=source, target=target) for target in self.successors(source)]
+    return frozenset(successors)
