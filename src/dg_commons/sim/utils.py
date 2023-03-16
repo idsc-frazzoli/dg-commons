6d312925543bfe65cff2c91ac9ebaf567f831c60
alezana <azanardi@ethz.ch>
2023-03-16 15:48:05 -0400

    who collided

--- src/dg_commons/sim/utils.py
+++ src/dg_commons/sim/utils.py
@@ -1,3 +1,7 @@
+from itertools import chain
+from typing import FrozenSet
+
+from dg_commons import PlayerName
 from dg_commons.sim.simulator import SimContext, Simulator
 
 __all__ = ["run_simulation"]
@@ -7,3 +11,8 @@ def run_simulation(sim_context: SimContext) -> SimContext:
     sim = Simulator()
     sim.run(sim_context)
     return sim_context
+
+
+def who_collided(sim_context: SimContext) -> FrozenSet[PlayerName]:
+    """Return the list of players that collided with the AV"""
+    return frozenset(chain.from_iterable(map(lambda report: report.players.keys(), sim_context.collision_reports)))
